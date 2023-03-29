55a0668ccc04d9cd65875f832266d1ee167ca146
USERNAME <shinozakiyume88@gmail.com>
2023-03-29 10:24:40 +0900

    propsに型を定義しよう

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,27 @@ type TodoType = {
   id: number;
   title: string;
   completed: boolean;
-}
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
-    axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      setTodos(res.data);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userid={todo.userId} />
+        <Todo
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
       ))}
     </div>
   );
